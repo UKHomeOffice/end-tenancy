a0d536a5a12c0ba9ac9595bd81c14e7131594693
joefitter <hello@joefitter.com>
2016-11-08 09:59:25 +0000

    stash

--- apps/end-tenancy/index.js
+++ apps/end-tenancy/index.js
@@ -29,12 +29,12 @@ module.exports = {
         }
       }],
       locals: {
-        section: 'what',
+        section: 'tenant-property',
         'report-link': 'https://eforms.homeoffice.gov.uk/outreach/lcs-reporting.ofml'
       }
     },
     '/report-nldp-date': {
-      controller: require('hof').controllers.date,
+      controller: controllers.date,
       fields: [
         'report-nldp-date',
         'report-nldp-date-day',
@@ -44,13 +44,16 @@ module.exports = {
       next: '/report-property-address',
       dateKey: 'report-nldp-date',
       locals: {
-        section: 'report-nldp-date'
+        section: 'tenant-property'
       }
     },
     '/report-property-address': {
       controller: require('./controllers/address-lookup'),
       addressKey: 'property-address',
-      next: '/tenant-details'
+      next: '/tenant-details',
+      locals: {
+        section: 'tenant-property'
+      }
     },
     '/tenant-details': {
       controller: require('./controllers/loop.js'),
@@ -100,19 +103,35 @@ module.exports = {
         field: 'add-another',
         value: 'yes'
       },
-      next: '/report-landlord-agent'
+      next: '/report-landlord-agent',
+      locals: {
+        section: 'tenant-property'
+      }
     },
     '/report-landlord-agent': {
-      next: '/landlord-address'
+      next: '/landlord-address',
+      locals: {
+        section: 'landlord-details'
+      }
     },
     '/landlord-address': {
       controller: require('./controllers/address-lookup'),
       addressKey: 'landlord-address',
       previousAddress: 'property-address',
-      next: '/confirm'
+      next: '/confirm',
+      locals: {
+        section: 'tenant-property'
+      }
     },
-    '/confirm': {},
+    '/confirm': {
+      controller: controllers.confirm,
+      next: '/confirmation',
+      fieldsConfig: require('./fields'),
+      emailConfig: require('../../config').email,
+      customerEmailField: 'email-address'
+    },
     '/request-property-address': {},
-    '/check-nldp-date': {}
+    '/check-nldp-date': {},
+    '/confirmation': {}
   }
 };
