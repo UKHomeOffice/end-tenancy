304a4f44e1434babb268b45b15535c53b7c9086e
GrahamOsifoHO <Graham.Osifo@digital.homeoffice.gov.uk>
2026-07-16 19:11:38 +0100

    update config playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig, devices } from '@playwright/test';
-
 import { defineBddConfig } from 'playwright-bdd';
 import dotenv from 'dotenv';
 
@@ -9,54 +8,58 @@ dotenv.config({ quiet: true });
 const port = Number(process.env.PLAYWRIGHT_PORT || 8080);
 const baseURL = process.env.PLAYWRIGHT_BASE_URL || `http://localhost:${port}`;
 
+
 const testDir = defineBddConfig({
-    features: 'e2e-tests/features/**/*.feature',
-    steps: [
-        'e2e-tests/steps/**/*.step.ts',
-        'e2e-tests/fixture/fixtures.ts'
-    ],
-    outputDir: '.features-gen',
+  features: 'e2e-tests/features/**/*.feature',
+  steps: [
+    'e2e-tests/steps/**/*.step.ts',
+    'e2e-tests/fixture/fixtures.ts',
+  ],
+  outputDir: '.features-gen',
 });
 
 export default defineConfig({
-    testDir,
-    timeout: 30000,
-    expect: {
-        timeout: 6000,
-    },
-    fullyParallel: true,
-    workers: process.env.CI ? 2 : 1,
-    forbidOnly: !!process.env.CI,
-    retries: process.env.CI ? 2 : 1,
-    reporter: [['html', { open: 'never' }], ['list']],
-    use: {
-        baseURL,
-        viewport: null,
-        screenshot: 'only-on-failure',
-        video: 'retain-on-failure',
-        trace: 'on-first-retry',
-        headless: false,
-    },
+  testDir,
+  timeout: 30000,
+  expect: {
+    timeout: 6000,
+  },
+  fullyParallel: true,
+  workers: process.env.CI ? 2 : 1,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 1,
+  reporter: [['html', { open: 'never' }], ['list']],
+  use: {
+    baseURL,
+    viewport: null,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    trace: 'on-first-retry',
+    headless: !!process.env.CI,
+  },
+
     webServer: process.env.PLAYWRIGHT_BASE_URL
-        ? undefined
-        : {
-            command: 'yarn start:dev',
-            port,
-            reuseExistingServer: !process.env.CI,
-            timeout: 120_000
+    ? undefined
+    : {
+      command: 'yarn start:dev',
+      port,
+      reuseExistingServer: !process.env.CI,
+      timeout: 120_000
+    },
+    
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      // use: { ...devices['Desktop Chrome'],
+      use: {
+        browserName: 'chromium',
+        launchOptions: {
+          args: ['--start-maximized'],
         },
-    /* Configure projects for major browsers */
-    projects: [
-        {
-            name: 'chromium',
-            use: {
-                browserName: 'chromium',
-                launchOptions: {
-                    args: ['--start-maximized'],
-                },
-                video: 'retain-on-failure', //Options => 'on', 'off', 'retain-on-failure' or 'on-first-retry'
-                screenshot: 'only-on-failure',
-            },
-        },
-    ],
+        video: 'on-first-retry', //Options => 'on', 'off', 'retain-on-failure' or 'on-first-retry'
+        screenshot: 'only-on-failure', //Options => 'on', 'off', 'only-on-failure' or 'on-first-retry'
+      },
+    },
+  ],
 });
