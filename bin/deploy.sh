44ad6a092c8001d075ed47f24401074d95baf2d7
adityababumallisettiHO <146218064+adityababumallisettiHO@users.noreply.github.com>
2026-07-20 13:38:40 +0100

    HOTFIX: Add back redis deployments to deploy scripts (#260)
    * Added redis runtime files to deployment process for branch and UAT environments.
    
    * Refactor Redis deployment and persistence handling
    
    * Refactor Redis deployment logic to handle image changes and PVC recreation. Update environment variable settings for Redis persistence based on the Kubernetes namespace.
    
    * Refactor Redis deployment and storage configuration
    
    * Updated Redis storage file and modified deployment logic for Redis persistence settings.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -7,31 +7,16 @@ export CONFIGMAP_VALUES=$HOF_CONFIG/configmap-values.yaml
 export NGINX_SETTINGS=$HOF_CONFIG/nginx-settings.yaml
 export FILEVAULT_NGINX_SETTINGS=$HOF_CONFIG/filevault-nginx-settings.yaml
 export FILEVAULT_INGRESS_EXTERNAL_ANNOTATIONS=$HOF_CONFIG/filevault-ingress-external-annotations.yaml
-
-kd='kd --insecure-skip-tls-verify --timeout 10m --check-interval 10s'
-
 export REDIS_PERSISTENCE_ENABLED=${REDIS_PERSISTENCE_ENABLED:-false}
 export REDIS_PERSISTENCE_ACCESS_MODES=${REDIS_PERSISTENCE_ACCESS_MODES:-ReadWriteOnce}
 export REDIS_PERSISTENCE_STORAGE_CLASS=${REDIS_PERSISTENCE_STORAGE_CLASS:-gp2-encrypted}
 export REDIS_PERSISTENCE_EXISTING_CLAIM=${REDIS_PERSISTENCE_EXISTING_CLAIM:-}
 
-REDIS_PERSISTENCE_ENABLED=$(echo "$REDIS_PERSISTENCE_ENABLED" | tr '[:upper:]' '[:lower:]')
+redis_storage_files='kube/redis/redis-persistent-volume.yml'
+redis_runtime_files='kube/redis/redis-service.yml -f kube/redis/redis-network-policy.yml -f kube/redis/redis-deployment.yml'
 
-deploy_redis() {
-  if [[ "$REDIS_PERSISTENCE_ENABLED" == 'true' ]] && [[ -z "$REDIS_PERSISTENCE_EXISTING_CLAIM" ]]; then
-    $kd -f kube/redis/redis-pvc.yml
-  fi
+kd='kd --insecure-skip-tls-verify --timeout 10m --check-interval 10s'
 
-  $kd -f kube/redis/redis-service.yml -f kube/redis/redis-network-policy.yml -f kube/redis/redis-deployment.yml
-}
-
-delete_redis() {
-  if [[ "$REDIS_PERSISTENCE_ENABLED" == 'true' ]] && [[ -z "$REDIS_PERSISTENCE_EXISTING_CLAIM" ]]; then
-    $kd --delete -f kube/redis/redis-pvc.yml
-  fi
-
-  $kd --delete -f kube/redis/redis-deployment.yml -f kube/redis/redis-service.yml -f kube/redis/redis-network-policy.yml
-}
 
 if [[ $1 == 'tear_down' ]]; then
   export KUBE_NAMESPACE=$BRANCH_ENV
@@ -45,37 +30,48 @@ if [[ $1 == 'tear_down' ]]; then
   exit 0
 fi
 
+
 export KUBE_NAMESPACE=$1
 export DRONE_SOURCE_BRANCH=$(echo $DRONE_SOURCE_BRANCH | tr '[:upper:]' '[:lower:]' | tr '/' '-')
 
-if [[ ${KUBE_NAMESPACE} == ${PROD_ENV} ]]; then
-  export REDIS_PERSISTENCE_ENABLED=true
-  export REDIS_PERSISTENCE_SIZE=10Gi
-elif [[ ${KUBE_NAMESPACE} == ${STG_ENV} ]]; then
-  export REDIS_PERSISTENCE_ENABLED=true
-  export REDIS_PERSISTENCE_SIZE=1Gi
+if [[ ${KUBE_NAMESPACE} == ${STG_ENV} || ${KUBE_NAMESPACE} == ${PROD_ENV} ]]; then
+  export REDIS_PERSISTENCE_ENABLED=${REDIS_PERSISTENCE_ENABLED:-true}
 else
   export REDIS_PERSISTENCE_ENABLED=false
 fi
+export REDIS_PERSISTENCE_ACCESS_MODES=${REDIS_PERSISTENCE_ACCESS_MODES:-ReadWriteOnce}
+export REDIS_PERSISTENCE_STORAGE_CLASS=${REDIS_PERSISTENCE_STORAGE_CLASS:-gp2-encrypted-eu-west-2b}
+export REDIS_PERSISTENCE_EXISTING_CLAIM=${REDIS_PERSISTENCE_EXISTING_CLAIM:-}
+export REDIS_PERSISTENCE_ANNOTATIONS_FILE=${REDIS_PERSISTENCE_ANNOTATIONS_FILE:-}
 
-REDIS_PERSISTENCE_ENABLED=$(echo "$REDIS_PERSISTENCE_ENABLED" | tr '[:upper:]' '[:lower:]')
+if [[ ${KUBE_NAMESPACE} == ${PROD_ENV} ]]; then
+  export REDIS_PERSISTENCE_SIZE=5Gi
+else
+  export REDIS_PERSISTENCE_SIZE=1Gi
+fi
+
+if [[ (${KUBE_NAMESPACE} == ${STG_ENV} || ${KUBE_NAMESPACE} == ${PROD_ENV}) && ${REDIS_PERSISTENCE_ENABLED} == "true" && -z "${REDIS_PERSISTENCE_EXISTING_CLAIM}" ]]; then
+  $kd -f $redis_storage_files
+fi
 
 if [[ ${KUBE_NAMESPACE} == ${BRANCH_ENV} ]]; then
   $kd -f kube/configmaps -f kube/certs
-  $kd -f kube/html-pdf -f kube/file-vault -f kube/app
+  $kd -f kube/html-pdf -f kube/file-vault 
+  $kd -f $redis_runtime_files -f kube/app
 elif [[ ${KUBE_NAMESPACE} == ${UAT_ENV} ]]; then
-  $kd -f kube/configmaps/configmap.yml  -f kube/app
+  $kd -f kube/configmaps/configmap.yml
   $kd -f kube/html-pdf -f kube/file-vault
+  $kd -f $redis_runtime_files -f kube/app
 elif [[ ${KUBE_NAMESPACE} == ${STG_ENV} ]]; then
   $kd -f kube/configmaps/configmap.yml  -f kube/app/service.yml
   $kd -f kube/app/ingress-internal.yml -f kube/app/networkpolicy-internal.yml
-  deploy_redis
-  $kd -f kube/html-pdf -f kube/file-vault -f kube/app/deployment.yml
+  $kd -f kube/html-pdf -f kube/file-vault 
+  $kd -f $redis_runtime_files -f kube/app/deployment.yml
 elif [[ ${KUBE_NAMESPACE} == ${PROD_ENV} ]]; then
   $kd -f kube/configmaps/configmap.yml  -f kube/app/service.yml
   $kd -f kube/app/ingress-external.yml -f kube/app/networkpolicy-external.yml
-  deploy_redis
-  $kd -f kube/html-pdf -f kube/file-vault -f kube/app/deployment.yml
+  $kd -f kube/html-pdf -f kube/file-vault 
+  $kd -f $redis_runtime_files -f kube/app/deployment.yml
 fi
 
 sleep $READY_FOR_TEST_DELAY
